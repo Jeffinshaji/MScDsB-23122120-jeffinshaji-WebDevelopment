d0c5d679c49dd3b49a0b53c25dcb12aa84a07067
Jeffin Shaji <jeffinshaji1997@gmail.com>
2023-11-25 11:32:14 +0530

    update

---
+++ script.js
@@ -0,0 +1,39 @@
+//var a=10;
+//let b=10;
+//const d=20;
+//var c;
+//c=a+b;
+//console.log(c);3
+
+function add()
+{
+   var a = document.getElementById("number1").value;
+   var b = document.getElementById("number2").value;
+var c=parseInt(a)+parseInt(b)
+
+document.getElementById("answer").value=c;
+}
+function sub()
+{
+   var a = document.getElementById("number1").value;
+   var b = document.getElementById("number2").value;
+var c=parseInt(a)-parseInt(b)
+
+document.getElementById("answer").value=c;
+}
+function multi()
+{
+   var a = document.getElementById("number1").value;
+   var b = document.getElementById("number2").value;
+var c=parseInt(a)*parseInt(b)
+
+document.getElementById("answer").value=c;
+}
+function divide()
+{
+   var a = document.get("number1").value;
+   var b = document.getElementById("number2").value;
+var c=parseInt(a)/parseInt(b)
+
+document.getElementById("answer").value=c;
+}
